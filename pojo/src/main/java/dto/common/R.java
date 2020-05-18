c68e3362ff978e1bf0b4ee17ca8eee20c895bca4
lyqingye <1316515719@qq.com>
2020-05-18 21:47:43 +0800

    ADD: 通用返回对象 和 通用分页请求

---
+++ pojo/src/main/java/dto/common/R.java
@@ -0,0 +1,100 @@
+package dto.common;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+/**
+ * 请求响应对象
+ *
+ * @author lyqingye
+ * @since 2020/3/24
+ */
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class R<T> {
+
+    /**
+     * 响应是否成功
+     */
+    private boolean success;
+
+    /**
+     * 错误代码
+     */
+    @JsonInclude(JsonInclude.Include.NON_DEFAULT)
+    private int errCode;
+
+    /**
+     * 错误消息
+     */
+    private String errMsg;
+
+    /**
+     * 错误详情
+     */
+    private String errDetail;
+
+    /**
+     * 响应数据
+     */
+    private T data;
+
+    /**
+     * 响应带数据的成功消息
+     *
+     * @param data 数据
+     * @param <E>  E
+     * @return 响应对象
+     */
+    public static <E> R<E> success(E data) {
+        R<E> vo = new R<>();
+        vo.setSuccess(true);
+        vo.setData(data);
+
+        return vo;
+    }
+
+    /**
+     * 响应成功消息
+     *
+     * @param <E> E
+     * @return 响应对象
+     */
+    public static <E> R<E> success() {
+        return success(null);
+    }
+
+    /**
+     * 响应成功消息
+     *
+     * @param <E> E
+     * @return 响应对象
+     */
+    public static <E> R<E> success(Void sign) {
+        return success();
+    }
+
+    /**
+     * 返回失败消息
+     * @param errMsg 失败消息
+     * @return 响应对象
+     */
+    public static R fail(String errMsg) {
+        return fail( errMsg,500);
+    }
+
+    /**
+     * 返回失败消息
+     * @param errMsg 错误消息
+     * @param errCode 错误码
+     * @return 响应对象
+     */
+    public static R fail(String errMsg, int errCode) {
+        R vo = new R();
+        vo.setSuccess(false);
+        vo.setErrCode(errCode);
+        vo.setErrMsg(errMsg);
+
+        return vo;
+    }
+}
---
+++ pojo/src/main/java/dto/common/RequestPage.java
@@ -0,0 +1,61 @@
+package dto.common;
+
+import lombok.Data;
+
+/**
+ * 分页查询对象
+ *
+ * @author lyqingye
+ * @since 2020/4/1
+ */
+@Data
+public class RequestPage<T> {
+
+    /**
+     * 分页大小
+     */
+    private int limit;
+
+    /**
+     * 排序字段
+     */
+    private String sort;
+
+    /**
+     * 排序方式 AES，DESC
+     */
+    private String dir;
+
+    /**
+     * 起始索引
+     */
+    private int start;
+
+    /**
+     * 当前页
+     */
+    private int page;
+
+    /**
+     * 请求数据
+     */
+    private T data;
+
+    /**
+     * 获取页大小
+     *
+     * @return
+     */
+    public int getPageSize() {
+        return limit == 0 ? 20 : limit;
+    }
+
+    /**
+     * 获取页数
+     *
+     * @return 页数
+     */
+    public int getPageNo() {
+        return page < 1 ? 1 : page;
+    }
+}
