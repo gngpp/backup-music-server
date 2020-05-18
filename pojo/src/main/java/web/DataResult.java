3cd5a687084b7b64229d90a980b3cdc9b1945256
Ant <1596863112@qq.com>
2020-05-18 20:19:51 +0800

    ant

---
+++ pojo/src/main/java/web/DataResult.java
@@ -0,0 +1,8 @@
+package web;
+
+/**
+ * @author ant
+ * Create by Ant on 2020/5/18 下午7:36
+ */
+public class DataResult {
+}
---
+++ server/src/main/java/com/zf1976/server/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.zf1976.server.security;
+
+
+/**
+ * @author ant
+ * Create by Ant on 2020/5/18 下午5:15
+ */
+
+public class SecurityConfig {
+
+//    public static final Logger LOGGER= LoggerFactory.getLogger(SecurityConfig.class);
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder(){
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        //关闭csrf，开启跨域
+//        http.csrf().disable()
+//            .cors();
+//    }
+
+}
