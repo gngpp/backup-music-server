c5d630ff9602755c6c6f30e001b894a4a0355546
Ant <1596863112@qq.com>
2020-06-22 07:49:47 +0800

    update:更新会员接口

--- pojo/target/generated-sources/annotations/com/zf1976/pojo/common/convert/CommentConvertImpl.java
+++ pojo/target/generated-sources/annotations/com/zf1976/pojo/common/convert/CommentConvertImpl.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-06-21T16:56:04+0800",
+    date = "2020-06-21T21:59:10+0800",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 1.8.0_231 (Oracle Corporation)"
 )
 @Component
